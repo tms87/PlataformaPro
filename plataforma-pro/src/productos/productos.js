8422065440ab014f9b39dd046f52a71d45444863
rapamatiase <rapa.matias.e@gmail.com>
2019-11-20 09:03:11 -0300

    algun cambiom

--- plataforma-pro/src/productos/productos.js
+++ plataforma-pro/src/productos/productos.js
@@ -83,50 +83,50 @@ export default  function Producto(porps){
             <h1> Productos</h1>
             <CssBaseline />
             <div className={classes.root}>
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={handleChange}
-      
-        aria-label="disabled tabs example"
-        className={classes.tabs}
-        textColor="secondary"
-      >
-        <Tab label="Todos los productos" icon={<KitchenIcon classes={{root : classes.rootSvg}} />} classes={{wrapper: classes.wrapper /*, labelIcon: classes.labelIcon*/}}    {...a11yProps(0)} />
-        <Tab label="Grupo 1: Lecha y derivados" classes={{wrapper: classes.wrapper}}   icon={<KitchenIcon classes={{root : classes.rootSvg}} /> } {...a11yProps(1)} />
-        <Tab label="Grupo 2: Carne, pescado y huevo" classes={{wrapper: classes.wrapper}}  {...a11yProps(2)} />
-        <Tab label="Grupo 3: Papas, legumbres y futos secos" classes={{wrapper: classes.wrapper}}  {...a11yProps(3)} />
-        <Tab label="Grupo 4: Verduras y hortalizas" classes={{wrapper: classes.wrapper}}  {...a11yProps(4)} />
-        <Tab label="Grupo 5: Frutas" classes={{wrapper: classes.wrapper}}  {...a11yProps(5)} />
-        <Tab label="Grupo 6: Cereales y derivados, axucar y dulces" classes={{wrapper: classes.wrapper}}  {...a11yProps(6)} />
-        <Tab label="Grupo 7: Grasas, aceite y manteca" classes={{wrapper: classes.wrapper}}  {...a11yProps(7)} />
-      </Tabs>
-      <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
-      <TabPanel value={value} index={7}>
-        Item Eight
-      </TabPanel>
-    </div>
+              <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={value}
+                onChange={handleChange}
+              
+                aria-label="disabled tabs example"
+                className={classes.tabs}
+                textColor="secondary"
+              >
+                <Tab label="Todos los productos" icon={<KitchenIcon classes={{root : classes.rootSvg}} />} classes={{wrapper: classes.wrapper /*, labelIcon: classes.labelIcon*/}}    {...a11yProps(0)} />
+                <Tab label="Grupo 1: Lecha y derivados" classes={{wrapper: classes.wrapper}}   icon={<KitchenIcon classes={{root : classes.rootSvg}} /> } {...a11yProps(1)} />
+                <Tab label="Grupo 2: Carne, pescado y huevo" classes={{wrapper: classes.wrapper}}  {...a11yProps(2)} />
+                <Tab label="Grupo 3: Papas, legumbres y futos secos" classes={{wrapper: classes.wrapper}}  {...a11yProps(3)} />
+                <Tab label="Grupo 4: Verduras y hortalizas" classes={{wrapper: classes.wrapper}}  {...a11yProps(4)} />
+                <Tab label="Grupo 5: Frutas" classes={{wrapper: classes.wrapper}}  {...a11yProps(5)} />
+                <Tab label="Grupo 6: Cereales y derivados, axucar y dulces" classes={{wrapper: classes.wrapper}}  {...a11yProps(6)} />
+                <Tab label="Grupo 7: Grasas, aceite y manteca" classes={{wrapper: classes.wrapper}}  {...a11yProps(7)} />
+              </Tabs>
+              <TabPanel value={value} index={0}>
+                Item One
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                Item Two
+              </TabPanel>
+              <TabPanel value={value} index={2}>
+                Item Three
+              </TabPanel>
+              <TabPanel value={value} index={3}>
+                Item Four
+              </TabPanel>
+              <TabPanel value={value} index={4}>
+                Item Five
+              </TabPanel>
+              <TabPanel value={value} index={5}>
+                Item Six
+              </TabPanel>
+              <TabPanel value={value} index={6}>
+                Item Seven
+              </TabPanel>
+              <TabPanel value={value} index={7}>
+                Item Eight
+              </TabPanel>
+            </div>
         </Container>
     );
 }
