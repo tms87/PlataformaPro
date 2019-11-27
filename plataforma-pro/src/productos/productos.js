8248d179bd959dd65ad703920a82f2a635a09692
rapamatiase <rapa.matias.e@gmail.com>
2019-11-27 11:04:26 -0300

    Arreglando el marge, saque por error las lindas imagenes de ian, lo siento ian

--- plataforma-pro/src/productos/productos.js
+++ plataforma-pro/src/productos/productos.js
@@ -1,15 +1,12 @@
-
-import React, { useState, useEffect } from 'react';
+import Container from './../../node_modules/@material-ui/core/Container';
+import React from './../../node_modules/react';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import KitchenIcon from '@material-ui/icons/Kitchen';
-import UrlInteligente from '../url';
-import Grid from '@material-ui/core/Grid';
-
+import TabInfo from './TabInfo';
 
 
 function TabPanel(props) {
@@ -64,98 +61,33 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const url = UrlInteligente.obtenerUrl("productos", "/productos");
 export default function Producto(props) {
+
   const classes = useStyles();
-  const [value, setValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const [tabValue, setTabValue] = React.useState(0);
+
+  const handleChangeTab = (event, newtabValue) => {
+    setTabValue(newtabValue);
   };
-  const [data, setData] = useState([]);
-  const [hasError, setErrors] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [refresh, setRefresh] = useState(false);
-
-  let { modoPaciente } = props;
-
-  useEffect(() => {
-    fetchApi();
-    setRefresh(false);
-  }, [refresh]);
-
-  async function fetchApi() {
-    try {
-      setLoading(true);
-      const res = await fetch(url);
-      await res.json()
-        .then(json => { setData(json);});
-    } catch (e) {
-      setErrors(e);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  function toString(json) {
-    if (json != null) {
-      return JSON.stringify(json).replace(/"/g, '')
-    }
-    return "";
-  }
-    
-    
 
   return (
-    <div className={classes.root}>
+    <Container style={{ maxWidth: '100%', padding: 0 }}>
+      <div className={classes.root}>
         <Tabs
           orientation="vertical"
           variant="scrollable"
-          value={value}
-          onChange={handleChange}
-
-          aria-label="disabled tabs example"
-          className={classes.tabs}
-          textColor="secondary"
-        >
-          <Tab label="Todos los productos" icon={<KitchenIcon classes={{ root: classes.rootSvg }} />} classes={{ wrapper: classes.wrapper /*, labelIcon: classes.labelIcon*/ }}    {...a11yProps(0)} />
-          <Tab label="Grupo 1: Lecha y derivados" classes={{ wrapper: classes.wrapper }} icon={<KitchenIcon classes={{ root: classes.rootSvg }} />} {...a11yProps(1)} />
-          <Tab label="Grupo 2: Carne, pescado y huevo" classes={{ wrapper: classes.wrapper }}  {...a11yProps(2)} />
-          <Tab label="Grupo 3: Papas, legumbres y futos secos" classes={{ wrapper: classes.wrapper }}  {...a11yProps(3)} />
-          <Tab label="Grupo 4: Verduras y hortalizas" classes={{ wrapper: classes.wrapper }}  {...a11yProps(4)} />
-          <Tab label="Grupo 5: Frutas" classes={{ wrapper: classes.wrapper }}  {...a11yProps(5)} />
-          <Tab label="Grupo 6: Cereales y derivados, axucar y dulces" classes={{ wrapper: classes.wrapper }}  {...a11yProps(6)} />
-          <Tab label="Grupo 7: Grasas, aceite y manteca" classes={{ wrapper: classes.wrapper }}  {...a11yProps(7)} />
-        </Tabs>
-        <TabPanel value={value} index={0}>
-                <Grid container spacing={3}>
-              {data.map((item, key) =>
-
-                <Grid item xs={12}>
-                    {item.nombre}
-                </Grid>
-              )}
-            </Grid>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-      </TabPanel>
-        <TabPanel value={value} index={2}>
-          Item Three
-      </TabPanel>
-        <TabPanel value={value} index={3}>
-          Item Four
-      </TabPanel>
-        <TabPanel value={value} index={4}>
-          Item Five
-      </TabPanel>
-        <TabPanel value={value} index={5}>
-          Item Six
-      </TabPanel>
-        <TabPanel value={value} index={6}>
-          Item Seven
-      </TabPanel>
-        <TabPanel value={value} index={7}>
-          Item Eight
-      </TabPanel>
-    </div> );
+          value={tabValue}
+          onChange={handleChangeTab}
+          indicatorColor="primary"
+          className={classes.tabs}>
+          {TabInfo.map(i => <Tab label={i.label} icon={i.icon} classes={{ wrapper: classes.wrapper }} {...a11yProps(i.key)} />)}}
+          </Tabs>
+        {TabInfo.map(i =>
+          <TabPanel value={tabValue} index={i.key} key={i.key * 10}>
+            {i.label}
+          </TabPanel>
+        )}
+      </div>
+    </Container>
+  );
 }
