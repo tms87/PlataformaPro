0c6d6e3c275a956080fee6c290e416cc42c2709b
rapamatiase <rapa.matias.e@gmail.com>
2019-12-03 13:19:42 -0300

    Divide en tres el grafico, por un tema de unidades

--- plataforma-pro/src/users/Grafico.js
+++ plataforma-pro/src/users/Grafico.js
@@ -5,6 +5,7 @@ import React, { PureComponent, Component  } from 'react';
 
 
 
+    import Grid from '@material-ui/core/Grid';
 
 
 
@@ -13,9 +14,11 @@ export default class Grafico extends PureComponent {
 
   render() {
     return (
+      <div style={{marginTop:'5%'}}>
+         <Grid item xs={12}>
       <LineChart
         width={800}
-        height={500}
+        height={200}
         data={ this.props.data}
         margin={{
           top: 5, right: 30, left: 20, bottom: 5,
@@ -26,11 +29,46 @@ export default class Grafico extends PureComponent {
         <YAxis />
         <Tooltip />
         <Legend verticalAlign="top" height={36}/>
-        <Line type="monotone" name="Peso" dataKey="peso" stroke="#8884d8" activeDot={{ r: 8 }} />
-        <Line type="monotone" name="Masa muscular" dataKey="masa_muscular" stroke="#82ca9d" />
-        <Line type="monotone" name="Masa grasa" dataKey="masa_grasa" stroke="#f58742" />
-        <Line type="monotone" name="Altura" dataKey="altura" stroke="#ebd834" />
+        <Line type="monotone" name="Altura [cm]" dataKey="altura" stroke="#ebd834" />
       </LineChart>
+      </Grid>
+        <Grid item xs={12}>
+      <LineChart
+        width={800}
+        height={200}
+        data={ this.props.data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="fecha"  />
+        <YAxis />
+        <Tooltip />
+        <Legend verticalAlign="top" height={36}/>
+        <Line type="monotone" name="Peso [kg]" dataKey="peso" stroke="#8884d8" activeDot={{ r: 8 }} />
+      </LineChart>
+      </Grid>
+      <Grid item xs={12}>
+
+       <LineChart
+       width={800}
+       height={200}
+       data={ this.props.data}
+       margin={{
+         top: 5, right: 30, left: 20, bottom: 5,
+       }}
+     >
+       <CartesianGrid strokeDasharray="3 3" />
+       <XAxis dataKey="fecha"  />
+       <YAxis />
+       <Tooltip />
+       <Legend verticalAlign="top" height={36}/>
+       <Line type="monotone" name="Masa muscular [%]" dataKey="masa_muscular" stroke="#82ca9d" />
+       <Line type="monotone" name="Masa grasa [%]" dataKey="masa_grasa" stroke="#f58742" />
+     </LineChart>
+     </Grid>
+     </div>
     );
   }
 }
